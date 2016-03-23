417f83506e9754ce59e7d24f7fa6610b2111a932
laloge_h <hugo.laloge@epitech.eu>
2016-03-23 15:07:38 +0100

    Refactor Generator.py

--- Generator.py
+++ Generator.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3.4
+#!/usr/bin/env python3
 
 ########################################
 #------------Donjon & Python-----------#
@@ -12,8 +12,12 @@
 #-----------Import des modules---------#
 
 import pygame
-from pygame.locals import placerCoffre
+from pygame.locals import *
 from tkinter import Tk
+
+LENGTH = 13
+HEIGHT = 7
+N_ELEM = 6
 
 from MoteurGraphique import chargerTextures, chargerTile, block, iEntite
 
@@ -31,14 +35,16 @@ class Tile:
 
 #--------Définition des fonctions------#
 
+
 def grille(x, y):
     return (x*32 + 32, y*32 +32)
+
 
 def rafraichir():
     global fenetre, etage, fond, block, grille
     fenetre.blit(fond[etage], grille(-1, -1))
-    for y in range(7):
-        for x in range(13):
+    for y in range(HEIGHT):
+        for x in range(LENGTH):
             grilleTiles[y][x].majTexure(etage)
             fenetre.blit(grilleTiles[y][x].texture, grille(x, y))
     pygame.display.flip()
@@ -53,8 +59,8 @@ def changeValue(pos, pas):
     global grilleTiles
     x = (pos[0] - 32)//32
     y = (pos[1] - 32)//32
-    if x >= 0 and x <= 12 and y >=0 and y <=6 :
-        grilleTiles[y][x].valeur = (grilleTiles[y][x].valeur+pas)%6
+    if x >= 0 and x < LENGTH and y >=0 and y < HEIGHT :
+        grilleTiles[y][x].valeur = (grilleTiles[y][x].valeur+pas) % N_ELEM
 
 def enregistrer():
     """Enregistre la salle dans un fichier"""
@@ -62,42 +68,34 @@ def enregistrer():
     try:
         nom = int(input('Entrez un numéro : '))
         text = ''
-        for y in range(7):
-            for x in range(13):
+        for y in range(HEIGHT):
+            for x in range(LENGTH):
                 text += str(grilleTiles[y][x].valeur)
             text += '\n'
         fichier = open('Salles\\'+str(nom)+'.txt', mode='w', encoding='Utf-8')
         fichier.write(text)
         fichier.close()
-    except : None
+    except : pass
 
 def ouvrir():
     """Ouvre un fichier paterne"""
     global grille
     try:
-        fichier = open('Salles\\' + input('Entrez un nom de fichier : ') + '.txt', mode='r', encoding='Utf-8')
+        fichier = open('Salles/' + input('Entrez un nom de fichier : ') + '.txt', mode='r', encoding='Utf-8')
         text = fichier.readlines()
         fichier.close()
-        for y in range(7):
-            for x in range(13):
+        for y in range(HEIGHT):
+            for x in range(LENGTH):
                 grilleTiles[y][x].valeur = int(text[y][x])
         rafraichir()
-    except : None
+    except : pass
 
 def reset():
     global grilleTiles
-    for y in range(7):
-        for x in range(13):
+    for y in range(HEIGHT):
+        for x in range(LENGTH):
             grilleTiles[y][x].valeur = 0
     rafraichir()
-
-def changerPack():
-    global texturePack
-    texturePack = input('Entrez un nom de texture pack : ')
-    try:
-        texutre(texturePack)
-        rafraichir()
-    except : None
 
 #--------Définition des variables------#
 
@@ -150,7 +148,6 @@ while continuer:
             elif event.key == K_s : enregistrer()
             elif event.key == K_o : ouvrir()
             elif event.key == K_r : reset()
-            elif event.key == K_t : changerPack()
 
         elif event.type ==  MOUSEBUTTONDOWN:
             if pygame.mouse.get_pressed()[0] : changeValue(pygame.mouse.get_pos(), 1)
